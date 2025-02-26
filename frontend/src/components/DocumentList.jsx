52eabe9f1cf4a2b925d3b2d429d60e5d4cb85dae
SriHariniPro <143264370+SriHariniPro@users.noreply.github.com>
2025-02-26 13:24:53 +0530

    Update DocumentList.jsx

--- frontend/src/components/DocumentList.jsx
+++ frontend/src/components/DocumentList.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { List, ListItem, ListItemText } from '@mui/material';
+
+export default function DocumentList({ documents }) {
+  return (
+    <List>
+      {documents.map((doc, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={doc.name} />
+        </ListItem>
+      ))}
+    </List>
+  );
+}
 
