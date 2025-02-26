937918b8640b4dc12c57c60362a11875806357f1
SriHariniPro <143264370+SriHariniPro@users.noreply.github.com>
2025-02-26 14:06:22 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import App from './App';
 
 const theme = createTheme({
   palette: {
@@ -10,9 +13,11 @@ const theme = createTheme({
   }
 });
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
